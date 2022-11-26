a60579981b5b862eeb887774d0e6991a02f17f2e
A.N <77659600+Barking-Owl@users.noreply.github.com>
2022-11-25 23:52:24 -0600

    Implement menu screen
    Finishing up relics + health mechanic now the enemies remain (in terms of staging) as well as the Help/Credits screen. Due to the amount of other assets used. Did work with the double doors and will replace the current single ones with it

---
+++ UnityAgonDray/Assets/Scripts/CanvasScripts/StartCanvas.cs
@@ -0,0 +1,40 @@
+/***
+ * Author: Andrew Nguyen
+ * Created: 25 November 2022
+ * Modified: 25 November 2022
+ * Description: Manages the start menu of the game
+ ***/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartCanvas : MonoBehaviour
+{
+    public string levelName;
+    public string helpScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void GameHelp()
+    {
+        SceneManager.LoadScene(helpScreen);
+    }
+}
