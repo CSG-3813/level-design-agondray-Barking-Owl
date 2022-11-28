d8fcef420d3bd2abf876d17e29e985c7c90bb6c9
A.N <77659600+Barking-Owl@users.noreply.github.com>
2022-11-28 17:34:03 -0600

    Spinning and more lights

---
+++ UnityAgonDray/Assets/Scripts/GameScripts/BossField.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossField : MonoBehaviour
+{
+    public GameObject Stone;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
